a23e1a28ab9838aec7a2e276a2be9cafc4956097
nandemotoken <nandemotoken@gmail.com>
2021-05-01 02:48:19 +0000

    demo factory

---
+++ assets/js/buttons.js
@@ -0,0 +1,99 @@
+
+async function contract_deployer(){
+    console.log(network.chainId)
+    
+    if (contractaddress) {
+        window.alert("このネットワークでコントラクトを再生成するには\nまず右上のResetを押してください。\n不明点は開発スタッフにお問い合わせください");
+        return;
+    }
+    
+    
+    factory = new ethers.ContractFactory(abi, bytecode, signer);
+    contract = await factory.deploy();   
+    console.log(contract.address);
+    contractaddress = contract.address;
+    await localStorage.setItem( network.name , contractaddress );
+    $("#transactionmodal").modal('show');
+    bar = document.getElementById("transactionprogressbar");
+    setInterval( function(){  bar.innerHTML = bar.innerHTML + "." ; if (bar.innerHTML.length > 80){ bar.innerHTML = "." } } , 3 * 1000 );
+    
+    await contract.deployTransaction.wait();
+    setTimeout( function(){location.reload()} , 5 * 1000 );
+    
+}
+
+async function mint_button(){
+        
+    if ( !contractaddress ){
+        window.alert("まず①create contractを実施してください");
+        return;
+    }
+        
+    await contract.mint();
+  
+    $("#transactionmodal").modal('show');
+    bar = document.getElementById("transactionprogressbar");
+    setInterval( function(){  bar.innerHTML = bar.innerHTML + "." ; if (bar.innerHTML.length > 80){ bar.innerHTML = "." } } , 3 * 1000 );
+    
+    contract.on("Mint", () => {
+        setTimeout( function(){location.reload()} , 5 * 1000 );
+    });
+    
+}
+
+async function setmetadata_button(){
+    
+    nftnumber = document.getElementById("nftnumber3").value
+    ipfshash = document.getElementById("ipfshash").value;
+    
+    if ( !contractaddress ){
+        window.alert("まず①create contractを実施してください");
+        return;
+    }
+    
+    console.log(ipfshash);
+    if (ipfshash.charAt(0) !== "Q"){
+        window.alert("IPFSハッシュでない値が入っている可能性が高いです\n再度見直してください")
+        return;
+    }
+    
+    await contract.setTokenURI( nftnumber , "ipfs://" + ipfshash).catch(() => window.alert("エラー：永続化されたNFTを変更しようとしていませんか？"));
+    
+    //localStorage.setItem( "lastsethash" , ipfshash );
+    
+    
+    $("#transactionmodal").modal('show');
+    bar = document.getElementById("transactionprogressbar");
+    setInterval( function(){  bar.innerHTML = bar.innerHTML + "." ; if (bar.innerHTML.length > 80){ bar.innerHTML = "." } } , 3 * 1000 );
+    
+    contract.on("SetTokenURI", ( _num , _uri ) => {
+        setTimeout( function(){location.reload()} , 5 * 1000 );
+    });
+
+}
+
+async function finalizeNFT(){
+    nftnumber4 = document.getElementById("secureNFTnumber4").value
+
+    
+    await contract.secureCryptoArt( nftnumber4 );
+  
+    $("#transactionmodal").modal('show');
+    bar = document.getElementById("transactionprogressbar");
+    setInterval( function(){  bar.innerHTML = bar.innerHTML + "." ; if (bar.innerHTML.length > 80){ bar.innerHTML = "." } } , 3 * 1000 );
+    
+    contract.on("SecureCryptoArt", ( _num ) => {
+        setTimeout( function(){location.reload()} , 5 * 1000 );
+    });
+    
+}
+
+async function contractlocalstragereset(){
+    ans = window.confirm("NFT Contract Addressを再設定するためにブラウザをリセットしますか？")
+    if ( !ans ){ return; }
+    
+    await localStorage.removeItem( network.name );
+    
+    window.location.reload();
+}
+
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,88 +1,73 @@
 
-
-
 window.onload = async function() {
-    
-    window.ethereum.enable();
-    
-    ethereum.on('chainChanged', (_chainId) => window.location.reload());
 
     console.log("IPFS_Hash_example: QmVNKKhPomQc4fHYvdiKBwFXdHKvSWu8Rkm7pfD1Db8f25")
     
+    await initializeMetaMask();
+    await setVisual();
+    await setOpenSeaLink();
+    await setNFTSecureLink();
+}
+
+
+async function initializeMetaMask(){
+    window.ethereum.enable();
+    ethereum.on('chainChanged', (_chainId) => window.location.reload());
     provider = await new ethers.providers.Web3Provider(window.ethereum);
     signer = await provider.getSigner();
     network = await provider.getNetwork();
-    
-    
-    
     contractaddress = localStorage.getItem(network.name);
-    
-    
+
+    //chainIdで表示
     document.getElementById("networkstatus").innerHTML = `現在のネットワーク：${network.chainId}` 
     
+    //もし別名があればそれで表示
     if (chainId_networkMap.has( network.chainId )){
         document.getElementById("networkstatus").innerHTML = `現在のネットワーク：${chainId_networkMap.get(network.chainId)}`
-    }
+    }    
 
+    checkNextNFTNumber();
 
-    // lastsethash = localStorage.getItem( "lastsethash");
-    
-    // if (lastsethash) {
-    //     document.getElementById("ipfshash").value = lastsethash;
-    // }
-    
+}
+
+async function checkNextNFTNumber(){
+    //console.log(contractaddress);
     if ( !contractaddress ){
-        //document.getElementById("NextNftNumber").innerHTML = "no contract created"    
-        return;
+    document.getElementById("makecontractheadingrow").classList.remove("deploy");
+    document.getElementById("makecontractbuttonrow").classList.remove("deploy");
+    document.getElementById("mint_button").classList.remove("deploy");
+    return;
     }
     
     document.getElementById("nftcontractaddress").innerHTML = `NFT Contract Address : ${contractaddress}`
     
     contract = await new ethers.Contract( contractaddress , abi , signer );
-    nftnumber = await contract.getnumber()
-    document.getElementById("NextNftNumber").innerHTML = `Next NFT Number : ${parseInt(nftnumber._hex) + 1}`;
-    
+    nftnumber = await contract.number();
+    document.getElementById("NextNftNumber").innerHTML = `Next NFT Number : ${nftnumber}`;
 }
 
 
 
+async function setVisual(){
+    opensea = document.getElementById("opensea");
+//    console.log("onload ok")
+    opensea.addEventListener("mouseover", function( event ) {
+    opensea.style.opacity=1; 
+    }, false);    
+    opensea.addEventListener("mouseleave", function( event ) {
+    opensea.style.opacity=0.3; 
+    }, false);    
+}
 
-// function addattribute(){
-//     //console.log("attribute");
-//     attributes_table = document.getElementById("attributes_table");
-//     var row = attributes_table.insertRow(0);
-//     var cell1 = row.insertCell(0);
-//     var cell2 = row.insertCell(1);
-//     var cell3 = row.insertCell(2);
-//     cell1.innerHTML = "string";
-//     cell2.innerHTML = document.getElementById("trait_type").value;
-//     cell3.innerHTML = document.getElementById("value").value;
-// };
+async function setOpenSeaLink(){
+    document.getElementById("opensealink").href = chainId_explorerMap.get(network.chainId) + "account";
+}
 
 
-// function set_nft_metadata_to_ipfs(){
-//     metadatajson = {};
-//     metadatajson.name = document.getElementById("name").value;
-//     metadatajson.description = document.getElementById("description").value;
-//     //metadatajson.image = "ipfs://" + document.getElementById("image").value;
-//     metadatajson.image = "ipfs://" + "QmYnHs7qT4ok8RhZHCBqU7V6UauBZFFjAJVVvZnvBhaTEJ";
-//     let attributes_length = document.getElementById("attributes_table").rows.length;
-    
-//     attrs = [];    
-//     for (let i = 0 ; i < attributes_length ; i++ ){
-//         _trait_type = document.getElementById("attributes_table").rows[i].cells[1].innerText;
-//         // console.log(_trait_type)
-//         _value = document.getElementById("attributes_table").rows[i].cells[2].innerText;
-//         attrs.push({ "trait_type" : _trait_type , "value" : _value });
-//         //console.log("a")        
-//     } 
+async function setNFTSecureLink(){
+    document.getElementById("secureNFTnumber4").addEventListener("change",function () {
+        document.getElementById("secureNFTLink").innerHTML = `${chainId_explorerMap.get(network.chainId)}assets/${contractaddress}/${document.getElementById("secureNFTnumber4").value}`
+    document.getElementById("secureNFTLink").href = `${chainId_explorerMap.get(network.chainId)}assets/${contractaddress}/${document.getElementById("secureNFTnumber4").value}`
 
-//     metadatajson.attributes = attrs;
-
-//     metadatatext = JSON.stringify(metadatajson);
-    
-//     console.log(metadatatext);
-    
-//     //console.log(metadatajson);
-// }
-
+    })
+}
